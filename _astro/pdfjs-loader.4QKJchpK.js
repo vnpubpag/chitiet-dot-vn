9624704efd5dcdb6ee88d00bb5332e41f678c8e8
sonnt <sonnt148@gmail.com>
2026-06-30 22:33:35 +0700

    Deploy: 2026-06-30 22:33:35

--- _astro/pdfjs-loader.4QKJchpK.js
+++ _astro/pdfjs-loader.4QKJchpK.js
@@ -1,22 +1,2 @@
 const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["_astro/pdf.D_HeJB4L.js","_astro/preload-helper.L5lOfJxi.js"])))=>i.map(i=>d[i]);
-import { t as e } from "./preload-helper.L5lOfJxi.js";
-let n;
-let __tla = (async ()=>{
-    var t = null;
-    n = function() {
-        return t ||= (async ()=>{
-            let [t, n] = await Promise.all([
-                e(()=>import(`./pdf.D_HeJB4L.js`).then(async (m)=>{
-                        await m.__tla;
-                        return m;
-                    }), __vite__mapDeps([0,1])),
-                e(()=>import(`./pdf.worker.min.DAx94mNI.js`).then(async (m)=>{
-                        await m.__tla;
-                        return m;
-                    }).then((e)=>e.default), [])
-            ]);
-            return t.GlobalWorkerOptions.workerSrc = n, t;
-        })(), t;
-    };
-})();
-export { n as t, __tla };
+import{t as e}from"./preload-helper.L5lOfJxi.js";var t=null;function n(){return t||=(async()=>{let[t,n]=await Promise.all([e(()=>import(`./pdf.D_HeJB4L.js`),__vite__mapDeps([0,1])),e(()=>import(`./pdf.worker.min.DAx94mNI.js`).then(e=>e.default),[])]);return t.GlobalWorkerOptions.workerSrc=n,t})(),t}export{n as t};
