c0879788913e9c9760c564a46410fe98927cab86
sonnt <sonnt148@gmail.com>
2026-09-04 10:48:36 +0700

    Deploy: 2026-09-04 10:48:35

--- sw.js
+++ sw.js
@@ -22,7 +22,7 @@
  * runtime_architecture.md muc PWA.
  */
 
-const VERSION = '7a60ea207547';
+const VERSION = '345397924637';
 const CACHE_NAME = 'ct-pwa-' + VERSION;
 const PRECACHE = ["/offline/","/manifest.webmanifest","/logo.png","/favicon.ico","/favicon.svg","/images/pwa/icon-192.png","/images/pwa/icon-512.png","/images/pwa/icon-maskable-512.png","/images/pwa/apple-touch-icon.png","/_astro/RivePlayerWidget.CCnsejXS.css","/_astro/themes.CRzWqwNz.css","/_astro/layout.Cu1Rr4YZ.css","/_astro/RivePlayerWidget.astro_astro_type_script_index_0_lang.BpFoMc76.js","/_astro/PwaSetup.astro_astro_type_script_index_0_lang.C7A9KLF4.js"];
 const OFFLINE_URL = '/offline/';
